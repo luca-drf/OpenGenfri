8f19cb4d5315262425b92c13e7b4db4f46d40019
Luca Da Rin Fioretto <radome@gmail.com>
2014-12-29 14:09:58 +0100

    Integrated django auth system. Added few basic templates

--- pos/pos/urls.py
+++ pos/pos/urls.py
@@ -1,7 +1,16 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.contrib.auth.views import login, logout, password_change
+from django.views.generic.base import RedirectView, TemplateView
 
 urlpatterns = patterns('',
+    url(r'^$', RedirectView.as_view(url='webpos/'), name='index'),
+    url(r'^login/$', login, {'template_name': 'login.html'}, name='login'),
+    url(r'^logout/$', logout, {'template_name': 'logout.html'}, name='logout'),
+    url(r'^chpwd/$', password_change, {'template_name': 'change_pass.html',
+                                       'post_change_redirect': 'success/'},
+                                      name='chpwd'),
+    url(r'^chpwd/success/$', TemplateView.as_view(template_name='success.html')),
     url(r'^webpos/', include('webpos.urls', namespace="webpos")),
     url(r'^admin/', include(admin.site.urls)),
 )
--- pos/webpos/urls.py
+++ pos/webpos/urls.py
@@ -3,4 +3,5 @@ from webpos import views
 
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
+    url(r'^refresh/$', views.refresh_quantities, name='refresh'),
 )
